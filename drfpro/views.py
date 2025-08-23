848e96966c41c7e6b77ae306e03521ae36389433
snehasistripathy44 <snehasistripathy44@gmail.com>
2025-08-24 01:59:27 +0530

    Update views nothing added test

--- drfpro/views.py
+++ drfpro/views.py
@@ -6,7 +6,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
-
 class Curdapi(ModelViewSet):
     def addpost(self,request):
         data = request.data
